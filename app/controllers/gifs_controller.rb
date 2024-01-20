dd80f2d52fcef910459171d85ed8c90733a32852
lafe <0lafethehorsescrub@gmail.com>
2024-01-20 13:48:21 -0600

    funniest gif shenanigans

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,19 +1,16 @@
 class GifsController < ApplicationController
-  def index
-    byebug
-    p request.ip
-    if true
-      send_file('app/assets/images/favicon.png', type: 'image/png', disposition: 'inline')
-    else
-      redirect_to 'https://github.com/mrcreepysos/msga/archive/refs/heads/main.zip', allow_other_host: true
-    end
-  end
+  # def index
+  #   byebug
+  #   p request.ip
+  #   if true
+  #     send_file('app/assets/images/favicon.png', type: 'image/png', disposition: 'inline')
+  #   else
+  #     redirect_to 'https://github.com/mrcreepysos/msga/archive/refs/heads/main.zip', allow_other_host: true
+  #   end
+  # end
 
   def show
-    p request.ip
-    p request.remote_host
-    p request.user_agent
-    if true
+    if request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp')
       send_file('app/assets/images/favicon.png', type: 'image/png', disposition: 'inline')
     else
       redirect_to 'https://github.com/mrcreepysos/msga/archive/refs/heads/main.zip', allow_other_host: true
