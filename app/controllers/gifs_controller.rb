de85e4e1a6eea8436cbd33dcec26660b0b03f20e
lafe <0lafethehorsescrub@gmail.com>
2024-01-20 14:01:49 -0600

    logger

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,5 +1,6 @@
 class GifsController < ApplicationController
   def show
+    p request.user_agent
     if request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp')
       send_file('app/assets/images/msga.png', type: 'image/png', disposition: 'inline')
     else
