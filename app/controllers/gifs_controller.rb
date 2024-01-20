49b52baf808dc7c84b1aaaa9c6817ee66c56efa7
lafe <0lafethehorsescrub@gmail.com>
2024-01-20 13:52:29 -0600

    gif download working

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,17 +1,7 @@
 class GifsController < ApplicationController
-  # def index
-  #   byebug
-  #   p request.ip
-  #   if true
-  #     send_file('app/assets/images/favicon.png', type: 'image/png', disposition: 'inline')
-  #   else
-  #     redirect_to 'https://github.com/mrcreepysos/msga/archive/refs/heads/main.zip', allow_other_host: true
-  #   end
-  # end
-
   def show
     if request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp')
-      send_file('app/assets/images/favicon.png', type: 'image/png', disposition: 'inline')
+      send_file('app/assets/images/msga.gif', type: 'image/gif', disposition: 'inline')
     else
       redirect_to 'https://github.com/mrcreepysos/msga/archive/refs/heads/main.zip', allow_other_host: true
     end
