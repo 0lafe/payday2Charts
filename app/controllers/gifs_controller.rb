b31be7f1263a495b2d061ed6c3e9db36504fbf1b
lafe <0lafethehorsescrub@gmail.com>
2024-05-28 13:43:14 -0500

    whoops

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,13 +1,10 @@
 class GifsController < ApplicationController
-  def index
-
-  end
+  def index; end
   
   def show
-    # @gif = DownloadGif.find(params[:id])
     @gif = DownloadGif.find_by(name: params[:id])
 
-    if true || request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp') || request.user_agent.include?('Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0')
+    if request.user_agent.include?('Discordbot') || request.user_agent.include?('discordapp') || request.user_agent.include?('Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0')
       send_file(@gif.gif_path, type: 'image/gif', disposition: 'inline')
     else
       redirect_to @gif.url, allow_other_host: true
