075e731c0f1737a76ebb0841bef279f23bd3dbd5
lafe <0lafethehorsescrub@gmail.com>
2025-02-01 14:21:15 -0600

    update to rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_05_084541) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_01_194922) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
