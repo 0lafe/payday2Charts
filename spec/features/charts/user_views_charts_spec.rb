1f7fa33b33bcb0f189488da81be1294482787e6b
lafe <0lafethehorsescrub@gmail.com>
2025-02-05 17:03:18 -0600

    more tests

--- spec/features/charts/user_views_charts_spec.rb
+++ spec/features/charts/user_views_charts_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "Get Charts" do
 
   before(:each) do
     visit root_path
+    expect(page).to have_selector(".chart-description") # wait for charts to load in
   end
 
   context "When a user views charts" do
@@ -12,11 +13,21 @@ RSpec.feature "Get Charts" do
       expect(page).to have_selector(".data-table", count: 6)
     end
 
+    scenario "They can scroll" do
+      find('#next-button').click
+      expect(page).to have_content("#7 Overall")
+      expect(page).to have_content("#12 Overall")
+    end
+
     scenario "They can search for melee weapons", js: true do
-      find("#search_field").send_keys("Two Handed Great Ruler")
-      select "Weapon Kills", from: "statistic_choice"
+      fill_in "search_field", with: "Two Handed Great Ruler"
       expect(page).to have_selector(".data-table", count: 1)
-      expect(page).to have_content("Two handed Great Ruler")
+      expect(page).to have_content("Two Handed Great Ruler")
+    end
+
+    scenario "They can change the viewed statistic" do
+      select2_select(".select2", "Perkdeck Used")
+      expect(page).to have_content(" (Deck)")
     end
   end
 end
