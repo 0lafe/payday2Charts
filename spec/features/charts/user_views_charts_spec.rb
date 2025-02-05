18316fc6c3f2294359a9c756881ab9f63bd8c183
lafe <0lafethehorsescrub@gmail.com>
2025-02-05 12:19:23 -0600

    first test

--- spec/features/charts/user_views_charts_spec.rb
+++ spec/features/charts/user_views_charts_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Get Charts" do
+
+  before(:each) do
+    visit root_path
+  end
+
   context "When a user views charts" do
     scenario "They see the melee chart" do
-      visit "/"
+      expect(page).to have_content("Melee Kills")
+      expect(page).to have_selector(".data-table", count: 6)
+    end
+
+    scenario "They can search for melee weapons", js: true do
+      find("#search_field").send_keys("Two Handed Great Ruler")
+      expect(page).to have_selector(".data-table", count: 1)
+      expect(page).to have_content("Two handed Great Ruler")
     end
   end
 end
