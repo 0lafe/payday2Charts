84b03a8f9af73032bca941b967b38abe137ae9df
lafe <0lafethehorsescrub@gmail.com>
2025-02-14 14:03:47 -0600

    login confirmation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     identity = params["openid.identity"]
     steam_id = identity && identity.gsub("https://steamcommunity.com/openid/id/", "")
     session[:steam_id] = steam_id
-    redirect_to root_path
+    redirect_to root_path, notice: "Logged In Successfully"
   end
 
   def new; end
