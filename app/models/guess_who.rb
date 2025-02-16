b03235281a284fd235f6d2b3e70c512b940fdc28
lafe <0lafethehorsescrub@gmail.com>
2025-02-16 00:45:20 -0600

    add skins guess who

--- app/models/guess_who.rb
+++ app/models/guess_who.rb
@@ -10,7 +10,8 @@ class GuessWho < ApplicationRecord
       ["Weapon", "weapon"],
       ["Melee", "melee"],
       ["Content Creators", "content_creators"],
-      ["Characters", "characters"]
+      ["Characters", "characters"],
+      ["Skins", "skins"],
     ]
   end
 
@@ -34,6 +35,8 @@ class GuessWho < ApplicationRecord
       self.items = lists["content_creators_list"].sample(24)
     elsif game_type == "characters"
       self.items = lists["characters_list"].sample(24)
+    elsif game_type == "skins"
+      self.items = lists["skins"].keys.sample(24)
     else
       item_stats = []
       if game_type == "mask"
@@ -44,6 +47,27 @@ class GuessWho < ApplicationRecord
         item_stats = get_stat_names("melee_kills").sample(24)
       end
       self.items = item_stats.map {|stat| Localizer.generate_image_url(stat) }
+    end
+  end
+
+  def self.skin_data(skin_id)
+    @lists ||= JSON.parse(File.read("./app/models/guess_who_lists.json"))
+    @lists["skins"][skin_id]
+  end
+
+  def self.skin_background(skin_id)
+    rarity = skin_data(skin_id)["rarity"]
+    case rarity
+    when "common"
+      "https://fbi.paydaythegame.com/assets/img/weapons/skins/rarity-1.png"
+    when "uncommon"
+      "https://fbi.paydaythegame.com/assets/img/weapons/skins/rarity-2.png"
+    when "rare"
+      "https://fbi.paydaythegame.com/assets/img/weapons/skins/rarity-3.png"
+    when "epic"
+      "https://fbi.paydaythegame.com/assets/img/weapons/skins/rarity-4.png"
+    when "legendary"
+      "https://fbi.paydaythegame.com/assets/img/weapons/skins/rarity-5.png"
     end
   end
 end
