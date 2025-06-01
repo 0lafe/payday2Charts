5044712023b1233cbc395b66bb8117c282f9c78a
lafe <0lafethehorsescrub@gmail.com>
2025-06-01 13:25:17 -0500

    sentry stuff

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.dig("sentry")
+  # get breadcrumbs from logs
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  # Add data like request headers and IP for users, if applicable;
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+end
