d5c0a4f834476e77e1d7bb7f23d7079b2569ffaa
lafe <0lafethehorsescrub@gmail.com>
2026-05-02 03:47:53 -0500

    auto lb updating

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 # Sending output to STDOUT for Docker
 set :output, { standard: '/proc/1/fd/1', error: '/proc/1/fd/2' }
 
-every 1.minutes do
-  rake "test:log"
+every 1.day, at: "12:00 am" do
+  rake "init_lb"
+end
+
+every 1.day, at: "12:00 pm" do
+  rake "init_lb"
 end
--- lib/tasks/fake_task.rake
+++
@@ -1,5 +0,0 @@
-namespace :test do
-  task log: :environment do
-      puts "[#{DateTime.now}]\tRunning from Cron!"
-  end
-end
